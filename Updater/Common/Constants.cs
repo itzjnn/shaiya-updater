3fec4f5f8a762fe8d7b3581e9d8516d321ee4151
jann <124282607+itzjnn@users.noreply.github.com>
2025-07-10 14:32:33 +0800

    Update constants and publish profile settings
    Bumped UpdaterVersion to 2 and changed the Source URL in Constants.cs. Updated publish profile and project user settings, including profile path and history metadata.

--- Updater/Common/Constants.cs
+++ Updater/Common/Constants.cs
@@ -2,8 +2,8 @@
 {
     static class Constants
     {
-        public const int UpdaterVersion = 1;
-        public const string Source = "https://kurtekat.github.io";
+        public const int UpdaterVersion = 2;
+        public const string Source = "https://itzjnn.github.io";
         public const string WebBrowserSource = "https://google.com";
     }
 }
